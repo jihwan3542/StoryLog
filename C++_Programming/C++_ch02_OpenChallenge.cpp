dd99ee581ecba799e47d11cd13476239f8b67d17
Jihwan_Kim <106225195+jihwan3542@users.noreply.github.com>
2025-05-24 14:52:45 +0900

    Add files via upload

---
+++ C++_Programming/C++_ch02_OpenChallenge.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	string s;
+	cout << "로미오>>";
+	cin >> s;
+	string t;
+	cout << "줄리엣>>";
+	cin >> t;
+
+	if (s == "가위")
+	{
+		if (t == "가위")
+			cout << "비겼습니다." << endl;
+		else if (t == "바위")
+			cout << "줄리엣이 이겼습니다." << endl;
+		else if (t == "보")
+			cout << "로미오가 이겼습니다." << endl;
+		else
+			cout << "줄리엣이 기권하였습니다." << endl;
+	}
+	else if (s == "바위")
+	{
+		if (t == "가위")
+			cout << "로미오가 이겼습니다." << endl;
+		else if (t == "바위")
+			cout << "비겼습니다." << endl;
+		else if (t == "보")
+			cout << "줄리엣이 이겼습니다." << endl;
+		else
+			cout << "줄리엣이 기권하였습니다." << endl;
+	}
+	else if (s == "보")
+	{
+		if (t == "가위")
+			cout << "줄리엣이 이겼습니다." << endl;
+		else if (t == "바위")
+			cout << "로미오가 이겼습니다." << endl;
+		else if (t == "보")
+			cout << "비겼습니다." << endl;
+		else
+			cout << "줄리엣이 기권하였습니다." << endl;
+	}
+	else
+	{
+		cout << "로미오가 기권하였습니다." << endl;
+		if (t != "가위" && t != "바위" && t != "보") {
+			cout << "줄리엣이 기권하였습니다." << endl;
+			cout << "비겼습니다." << endl;
+		}
+		else
+			cout << "줄리엣이 이겼습니다." << endl;
+
+	}
+}
